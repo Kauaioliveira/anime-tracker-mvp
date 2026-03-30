81a3a9b38d08a566b55eeae257aaa6bd9eec1706
Kauai Oliveira braga <kauaioliveira@outlook.com>
2026-03-30 08:14:29 -0300

    feat: campo de busca com useState

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,29 @@
+import { useState } from 'react'
 import Header from './Header.tsx'
 
 function App() {
+  const [searchQuery, setSearchQuery] = useState('')
+
   return (
     <main>
       <Header
         title="Anime Tracker"
         description="Acompanhe seus animes favoritos e torne sua experiência de assistir animes mais divertida."
       />
+
+      <section>
+        <label htmlFor="search">Buscar anime</label>
+        <input
+          id="search"
+          type="text"
+          placeholder="Digite o nome..."
+          value={searchQuery}
+          onChange={(e) => setSearchQuery(e.target.value)}
+        />
+        <p>
+          Você digitou: <strong>{searchQuery || '(nada ainda)'}</strong>
+        </p>
+      </section>
     </main>
   )
 }
