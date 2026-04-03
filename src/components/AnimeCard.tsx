a82c3e164a5164c0b5b1b87dfa92cf9c9a4e885e
Kauai Oliveira braga <kauaioliveira@outlook.com>
2026-04-03 16:27:08 -0300

    feat: add anime detail view on separate  route

--- src/components/AnimeCard.tsx
+++ src/components/AnimeCard.tsx
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom'
 import type { Anime } from '../types/anime.ts'
 
 type AnimeCardProps = {
@@ -5,20 +6,22 @@ type AnimeCardProps = {
 }
 
 export default function AnimeCard({ anime }: AnimeCardProps) {
-  const { title, images } = anime
+  const { title, images, mal_id } = anime
   const src = images.jpg.image_url
 
   return (
     <article className="anime-card">
-      <img
-        className="anime-card__img"
-        src={src}
-        alt=""
-        loading="lazy"
-        width={225}
-        height={318}
-      />
-      <h2 className="anime-card__title">{title}</h2>
+      <Link to={`/anime/${mal_id}`} className="anime-card__link">
+        <img
+          className="anime-card__img"
+          src={src}
+          alt=""
+          loading="lazy"
+          width={225}
+          height={318}
+        />
+        <h2 className="anime-card__title">{title}</h2>
+      </Link>
     </article>
   )
 }
